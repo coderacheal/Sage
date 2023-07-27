59c4a1910d71962ec808b04ba1f42bcc28335956
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-07-27 07:09:02 +0000

    Data from redux store working

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { useState } from 'react';
 import CryptoDetails from './cryptoDetails';
 import HomePage from './HomePage';
-import BarChart from './Barchart';
-import { UserData } from './Data';
 import Charts from './Charts';
 
 function App() {
-  // const [userData, setUserData] = useState({
-  //   labels: UserData.map((data) => data.year),
-  //   datasets: [
-  //     {
-  //       label: 'Users Gained',
-  //       data: UserData.map((data) => data.userGain),
-  //       backgroundColor: [
-  //         'rgba(75,192,192,1)',
-  //         '#ecf0f1',
-  //         '#50AF95',
-  //         '#f3ba2f',
-  //         '#2a71d0',
-  //       ],
-  //       borderColor: 'red',
-  //       borderWidth: 2,
-  //     },
-  //   ],
-  // });
   return (
     <div className="App">
       <BrowserRouter>
--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -32,7 +32,16 @@ const CryptoChart = () => {
     datasets: [
       {
         label: 'Percentage change of price',
-        data: UserData.map((data) => data.userGain),
+        data: [
+          marketData?.price_change_percentage_1h_in_currency?.usd,
+          marketData?.price_change_percentage_24h_in_currency?.usd,
+          marketData?.price_change_percentage_7d_in_currency?.usd,
+          marketData?.price_change_percentage_14d_in_currency?.usd,
+          marketData?.price_change_percentage_30d_in_currency?.usd,
+          marketData?.price_change_percentage_60d_in_currency?.usd,
+          marketData?.price_change_percentage_200d_in_currency?.usd,
+          marketData?.price_change_percentage_1y_in_currency?.usd,
+        ],
         backgroundColor: 'aqua',
       },
     ],
